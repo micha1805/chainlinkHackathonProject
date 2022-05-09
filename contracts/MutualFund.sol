eea49f919fe793af48f97f846af4f900af6d427c
Michel Plainchamp <michel.plainchamp@gmail.com>
2022-05-09 14:20:30 +0200

    removed some unclean and unfinished code, mainly functions for more advanced versions of the current code, GO BACK HERE FROM THE FUTURE TO GET THESE DRAFTS

--- contracts/MutualFund.sol
+++ contracts/MutualFund.sol
@@ -188,81 +188,6 @@ contract MutualFund is VRFConsumerBase, Ownable {
 	}
 
 
-	// function getRandomArrayOfJurees() private {
-
-	// 	// This function gets ONE random number from Chainlink VRF, converts it to an index number, from
-	// 	// then iterate to the N next adresses to get correct amount of accounts.
-	// 	// This is not optimal (each different group of jurees will always be very similar), also if you
-	// 	// create N accounts and enter the contract afterwards, you could easily get a huge lever in
-	// 	// a juree if all of them are selected to be juree.
-	// 	// This 'trick' is done mainly to reduce gas fee due to :
-	// 	// 1) big computations (a random subarray needs a lot of permutation computations) and
-	// 	// 2) multiple calls to Chainlik VRF to get multiple different indexes, we only us 1 call here.
-	// 	//
-	// 	// Next versions of this contract should get a better way to choose N different random users.
-
-
-	// 	// before everything :  prevent another computation :
-	// 	fund_state = FUND_STATE.COMPUTING_JUREES;
-
-	// 	// FIRST STEP : Get a random number
-
-	// 	bytes32 requestId = requestRandomness(keyHash, fee);
-	// 	// emit to event to track it :
-	// 	emit RequestedRandomness(requestId);
-
-
- //    // SECOND STEP : get a random array of users
- //    // delete jurees;
-
-
-
- //    // final step : reopenn the possibility to create a new juree.
- //    fund_state = FUND_STATE.READY;
- //  }
-
-
-  function checkIfIAmAJuree() public{
-  	// this function is called by the front to check if the user is in a new juree
-
-  	// loop on all the open request, and check if my address is there.
-  }
-
-  function voteOfJureeForARequest(uint256 indexRequest, bool _vote) public {
-  	// check if juree has already voted
-  	// if not
-  }
-
-  function modifyRequestVote(uint256 indexRequest) private {
-
-  }
-
-
-  function getOpenRequestsIndexes() public view returns(uint[] memory filteredRequestsIndexes){
-
-  	// uint256[] storage index_of_open_requests;
-  	// Request[] memory requestsTemp = new Request[](requests_number);
-  	// uint256 count;
-
-
-  	// for(uint256 i=0;i<requests_number;i++){
-  	// 	if(request_mapping[i].state == REQUEST_STATE.OPEN){
-   //          requestsTemp[count] = request_mapping[i]; //or whatever you want to do if it matches
-   //          count += 1;
-   //    }
-  	// }
-
-  	// filteredRequestsIndexes = new uint256[](count);
-  	// for(uint256 i = 0; i<count; i++){
-   //    filteredRequestsIndexes[i] = requestsTemp[i].index;
-   //  }
-
-   //  // the following array contains the indexes of all the open requests :
-  	return filteredRequestsIndexes;
-
-  }
-
-
   function checkIfUserBelongsToAJureeOfACertainRequest(uint256 _userAddress, uint256 _requestIndex) public view returns(bool _answer){
 
   	_answer = false;
@@ -279,26 +204,5 @@ contract MutualFund is VRFConsumerBase, Ownable {
 
   	return _answer;
   }
-
-	/////////////
-	// SETTERS //
-	/////////////
-
-	function modifyJureesNumber(uint256 _newJureesNumber) public onlyOwner {
-		jurees_number = _newJureesNumber;
-	}
-
-	function modifyMultiple(uint256 _newMultiple) public onlyOwner {
-		max_multiple = _newMultiple;
-	}
-
-	// in case of bug, to avoid a broken contract:
-	function forceSetting() public onlyOwner {
-		fund_state = FUND_STATE.READY;
-	}
-
-	// // To be understood :
-	// function checkEachMonth() private {} // Chainlink keeper CRON?
-	//
 
 }
