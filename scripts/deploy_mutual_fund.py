314192d243f18e5f22a77834b3f169bf8f339b7b
Michel Plainchamp <michel.plainchamp@gmail.com>
2022-05-19 10:27:43 +0200

    I can automatically push users into contract from deploy script, need some refactoring though

--- scripts/deploy_mutual_fund.py
+++ scripts/deploy_mutual_fund.py
@@ -1,5 +1,25 @@
+from brownie import accounts, MutualFund
 from scripts.helpful_scripts import deploy_mutual_fund
 
+def add_users_to_contract():
+	mutual_fund = MutualFund[-1]
+	accounts.load('user0')
+	accounts.load('user1')
+	accounts.load('user2')
+	accounts.load('user3')
+	accounts.load('user4')
+	accounts.load('user5')
+	accounts.load('user6')
+	accounts.load('user7')
+	accounts.load('user8')
+	accounts.load('user9')
+	i = 0;
+	for account in accounts:
+		print(f" account[{i}] = {account}")
+		i+=1
+		mutual_fund.enter({"from":account})
+	print(mutual_fund)
 
 def main():
-	deploy_mutual_fund()
+	# deploy_mutual_fund()
+	add_users_to_contract()
