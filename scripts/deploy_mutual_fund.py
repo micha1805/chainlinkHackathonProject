8aca0b96a19fef559e1e20ada8eb2281cee863d8
Michel Plainchamp <michel.plainchamp@gmail.com>
2022-05-19 11:31:52 +0200

    some refactoring in auto adding the accounts, but I still need to enter the passwords each time

--- scripts/deploy_mutual_fund.py
+++ scripts/deploy_mutual_fund.py
@@ -1,18 +1,27 @@
+from subprocess import Popen, PIPE
 from brownie import accounts, MutualFund
 from scripts.helpful_scripts import deploy_mutual_fund
 
+def load_account(index):
+
+	# load accounts from 'user0' to 'user9'
+	# YOU NEED TO HAVE LOADED THESE ACCOUNTS IN BROWNIE WITH :
+	# brownie accounts new user0
+	# brownie accounts new user1
+	# brownie accounts new user2
+	# etc.
+	# the passwords will still be asked while loading them though.
+
+	account_name = "user"+str(index)
+	accounts.load(account_name)
+
+
 def add_users_to_contract():
 	mutual_fund = MutualFund[-1]
-	accounts.load('user0')
-	accounts.load('user1')
-	accounts.load('user2')
-	accounts.load('user3')
-	accounts.load('user4')
-	accounts.load('user5')
-	accounts.load('user6')
-	accounts.load('user7')
-	accounts.load('user8')
-	accounts.load('user9')
+
+	for index in range(9):
+		load_account(index)
+
 	i = 0;
 	for account in accounts:
 		print(f" account[{i}] = {account}")
@@ -21,5 +30,5 @@ def add_users_to_contract():
 	print(mutual_fund)
 
 def main():
-	# deploy_mutual_fund()
+	deploy_mutual_fund()
 	add_users_to_contract()
