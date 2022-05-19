8fdbdb20b0b0f4837dbafd0deef8a897eb12bb24
Michel Plainchamp <michel.plainchamp@gmail.com>
2022-05-19 15:22:47 +0200

    empty app with DAppProvider working

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
+import { ChainId, DAppProvider } from "@usedapp/core"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DAppProvider config={{
+      // supportedChains: [ChainId.Kovan, ChainId.Rinkeby, 1337]
+      supportedChains: [ChainId.Kovan],
+      notifications: {
+        expirationPeriod: 1000,
+        checkInterval: 1000
+      }
+    }}>
+	    <div className="App">
+	      <header className="App-header">
+	        <img src={logo} className="App-logo" alt="logo" />
+	        <p>
+	          Edit <code>src/App.tsx</code> and save to reload.
+	        </p>
+	        <a
+	          className="App-link"
+	          href="https://reactjs.org"
+	          target="_blank"
+	          rel="noopener noreferrer"
+	        >
+	          Learn React
+	        </a>
+	      </header>
+	    </div>
+	   </DAppProvider>
   );
 }
 
